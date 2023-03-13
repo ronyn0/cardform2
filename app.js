bafdd9d7b5286ba0fc09f68b3fcc77f73ae6706c
ronyn0 <ronyn@librem.one>
2023-03-12 23:56:46 -0400

    set up layout.pug and added favorite icon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('express-favicon');
 
 var con = require('./database/mydb');
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/images/d20.png'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
