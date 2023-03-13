a194df379601cd74300df386b4d55641db346ee9
ronyn0 <ronyn@librem.one>
2023-03-13 01:54:52 -0400

    learning more sequelize and making a test model

---
+++ models/customers.js
@@ -0,0 +1,75 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require('../database/sequelize');
+
+const Customers = sequelize.define("Customers", {
+    id: {
+        type: Sequelize.INTEGER,
+        allownull: true,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allownull: true
+    },
+    address: {
+        type: Sequelize.STRING,
+        allownull: true
+    }
+ }, {
+    timestapes: false
+});
+
+// Customers model complete and lines up with the database
+
+/* now some commands to test the interaction, like sync
+
+sequelize.sync().then(() => {
+    console.log('Table Found!');
+    }).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
+
+// Customers.create makes a new object and inserts it 
+
+Customers.create({
+    name: 'Joe',
+    address: 'Fake Street 1'
+}).then(user => {
+    console.log(user.get({ plain: true }));
+}).catch(err => {
+    console.log(err);
+});
+
+// Customers.findAll is like a select * from with more info 
+
+Customers.findAll().then(customers => {
+    console.log(customers);
+}).catch(err => {
+    console.log(err);
+});
+
+// Customers.findOne is like a search for one item
+
+Customers.findOne({
+    where: { id: 18 }
+}).then(user => {
+    console.log(user.name);
+}).catch(err => {
+    console.log(err);
+});
+
+// update to update records, I'm running out of steam it's late
+
+Customers.update({
+    name: 'Joe'
+}, {
+    where: { id: 18 }
+}).then(() => {
+    console.log('Customer name updated.');
+}).catch(err => {
+    console.log(err);
+});
+
+// test the model
+console.log(Customers === sequelize.models.Customers);
+*/
