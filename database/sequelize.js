38714d59480eb816424552e4b306745cc51e72ce
ronyn0 <ronyn@librem.one>
2023-03-13 00:44:00 -0400

    added sequelized orm

---
+++ database/sequelize.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+   'mydb',
+   'ron',
+   'alligator',
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        socketPath: '/var/run/mysqld/mysqld.sock'
+      }
+    }
+  );
+
+sequelize.authenticate().then(() => {
+   console.log('Connection has been established successfully.');
+}).catch((error) => {
+   console.error('Unable to connect to the database: ', error);
+});
