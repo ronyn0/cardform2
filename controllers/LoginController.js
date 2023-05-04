64a1c317de2173be01455b02155a74937138158a
ronyn0 <ronyn@librem.one>
2023-05-04 00:58:13 -0400

    added accounts model

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -14,7 +14,7 @@ exports.auth = (req, res, next) => {
     // Ensure the input fields exists and are not empty
     if (username && password) {
         // Execute SQL query that'll select the account from the database based on the specified username and password
-        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+        connection.query('SELECT * FROM Accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
             // If there is an issue with the query, output the error
             if (error) throw error;
             // If the account exists
---
+++ models/Accounts.js
@@ -0,0 +1,67 @@
+const { Sequelize, DataTypes, SequelizeScopeError } = require("sequelize");
+async = require('async');
+const sequelize = require('../database/sequelize');
+
+const Accounts = sequelize.define("Accounts", {
+    id: {
+        type: Sequelize.INTEGER,
+        allownull: true,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: Sequelize.STRING,
+        allownull: false,
+        unique: true,
+        validate: {
+            is: {
+                args: /^[a-z0-9]+$/i, // string contains letters
+                msg: "Username can only contain letters and numbers."
+            },
+            len: [2,50]
+        }
+    },
+    password: {
+        type: Sequelize.STRING,
+        allownull: false,
+        vallidate: {
+            isAplhaNumeric: {
+                msg: "Password must contain letters and numbers."
+            }
+        }
+    },
+    email: {
+        type: Sequelize.STRING,
+        allownull: false,
+        vallidate: {
+            isEmail: {
+                msg: "Email must be an email format."
+            },
+            len: [2,100]
+        }
+    },
+    role: {
+        type: Sequelize.STRING,
+        allownull: true,
+        defaultValue: 'User'
+    }
+}, {
+    freezeTableName: true
+});
+
+/*
+Accounts.create({
+    username: 'test',
+    password: 'test',
+    email: 'test@test.com'
+}).then(user => {
+    console.log(user.get({ plain: true }));
+}).catch(err => {
+    console.log(err);
+});
+*/
+
+//Accounts.sync();  // no alter
+//Accounts.sync( {alter:true} );  // does alter
+
+module.exports = Accounts;
